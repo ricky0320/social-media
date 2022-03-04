29dd0eca8c11528ba56f8c98f3dc3df7bd6bc4a2
Ricky Leung <ricxx0320@gmail.com>
2022-03-04 10:31:51 -0800

    finished up models

--- models/User.js
+++ models/User.js
@@ -29,6 +29,7 @@ const UserSchema = new Schema(
     ],
     toJSON: {
       virtuals: true,
+      getters: true
     },
     id: false
   }
---
+++ models/index.js
@@ -0,0 +1,5 @@
+//require the user and thought file path/model
+const User = require("./User");
+const Thought = require("./Thought");
+
+module.exports = { User, Thought};
